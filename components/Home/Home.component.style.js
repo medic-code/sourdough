dc1cc391fa30c3df68eb9d4a0f8afed4c6c41577
medic code <u40as7@gmail.com>
2021-07-31 15:56:45 +0100

    styling setup

--- components/Home/Home.component.style.js
+++ components/Home/Home.component.style.js
@@ -1,9 +1,9 @@
 import {StyleSheet} from 'react-native';
-
+import theme from '../../styles/theme.style';
 export default StyleSheet.create({
   container: {
     flex:1,
-    padding: 20,
+    paddingVertical: theme.CONTAINER_PADDING,
     alignItems: 'center'
   }
 })
