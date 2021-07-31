959dc4913cb33466bf77388aa1992221156bc4c3
medic code <u40as7@gmail.com>
2021-07-31 15:44:28 +0100

    Home v1

---
+++ components/Home/Home.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, TextInput} from 'react-native'
+import styles from './Home.component.style';
+
+const Home = () => {
+  return(
+    <View style={styles.container}>
+      <Text> Sourdough </Text>
+    </View>
+  )
+}
+
+export default Home;
